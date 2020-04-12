31a7fc0d7b11286f187b09dae376a3820114c81a
jchaves07 <jorge.chaves@me.com>
2020-04-11 23:00:29 -0600

    Update Carousel.tsx

--- src/Carousel.tsx
+++ src/Carousel.tsx
@@ -18,7 +18,7 @@ export interface CarouselProps {
     autoplay?: boolean;
     autoplayTimeout?: number;
     slipFactor?: number;
-    animation?: (animate: Animated.Value, toValue: number, useNativeDriver: true,) => Animated.CompositeAnimation;
+    animation?: (animate: Animated.Value, toValue: number, useNativeDriver: false,) => Animated.CompositeAnimation;
     onPageChanged?: (index: number) => void;
     showsPageIndicator?: boolean;
     renderPageIndicator?: (config: PageIndicatorConfig) => JSX.Element;
